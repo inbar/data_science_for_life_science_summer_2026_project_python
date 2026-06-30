0c1e744c6865e90266c0f7152f605ba1bd5db406
Inbar Dagan <poupik@gmail.com>
2026-06-30 23:34:28 +0200

    Delete results folder

--- src/persistence/intermediates.py
+++
@@ -1,6 +0,0 @@
-def save_intermediate_dataset(dataset: MuData,
-                              ):
-    dataset_file_name = get_dataset_file_name()
-    dataset_file_path = PROCESSED_DATA_DIR_PATH / dataset_file_name
-
-    save_mudata_dataset_to_disk(dataset, dataset_file_path)
